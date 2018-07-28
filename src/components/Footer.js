2331d488bcbebb6bd0ffbb4a35b7772ea401140d
MIHAI POPESCU <mihai.ppscu@gmail.com>
2018-07-28 21:54:21 +0300

    add title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,12 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
+                        <li><a href="#" className="icon fa-facebook"><span className="label">Twitter</span></a></li>
+                        <li><a href="#" className="icon fa-instagram"><span className="label">Github</span></a></li>
                         <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
                     </ul>
                     <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                        <li>&copy; Floripedia</li>
                     </ul>
                 </div>
             </div>
