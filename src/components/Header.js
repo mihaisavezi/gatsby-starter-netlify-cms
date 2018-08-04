739ec98c61b3709c5ff796465a62e2125eef847c
MIHAI POPESCU <mihai.ppscu@gmail.com>
2018-08-04 19:50:32 +0300

    add svg logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Footer from './Footer'
-import logo from '../assets/images/logo.png'
+import logo from '../assets/images/logo.svg'
 
 class Header extends React.Component {
     render() {
