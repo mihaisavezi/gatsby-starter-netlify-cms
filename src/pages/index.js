39e4df65aed9b0f8c3c5c7ba6cd8d96599f6a317
MIHAI POPESCU <mihai.ppscu@gmail.com>
2019-01-30 21:32:41 +0200

    add text. format using prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,119 +7,185 @@ import Helmet from 'react-helmet'
 import Gallery from '../components/Gallery'
 
 const DEFAULT_IMAGES = [
-    { id: '1', src: null, thumbnail: null, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '2', src: null, thumbnail: null, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '3', src: null, thumbnail: null, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '4', src: null, thumbnail: null, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '5', src: null, thumbnail: null, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '6', src: null, thumbnail: null, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
-];
+  {
+    id: '1',
+    src: null,
+    thumbnail: null,
+    caption: 'Spring is in the air',
+    description:
+      '(lalele, frezii, eucalipt, muşchi de copac şi o bucată de lemn)',
+  },
+  {
+    id: '2',
+    src: null,
+    thumbnail: null,
+    caption: 'More delicate than cashmere',
+    description: '(hortensia, clopoţei de Texas, trandafiri vuvuzela, brunia)',
+  },
+  {
+    id: '3',
+    src: null,
+    thumbnail: null,
+    caption: 'No ordinary spring',
+    description: '(clopoţei de Texas, liliac)',
+  },
+  {
+    id: '4',
+    src: null,
+    thumbnail: null,
+    caption: 'Seduction',
+    description:
+      '(bujori, ranunculus, lalele ice cream, trandafiri, dusty miller)',
+  },
+  {
+    id: '5',
+    src: null,
+    thumbnail: null,
+    caption: 'You had me at hello',
+    description:
+      '(trandafiri de grădină, dalii, liliac japonez, leucadendron, eucalipt)',
+  },
+  {
+    id: '6',
+    src: null,
+    thumbnail: null,
+    caption: 'Together',
+    description:
+      '(halleborus, clopoţei de Texas, trandafiri vuvuzela, trandafiri tros)',
+  },
+]
 
 class HomeIndex extends React.Component {
+  constructor() {
+    super()
 
-    constructor() {
-        super();
-
-        this.state = {
-            lightboxIsOpen: false,
-            currentImage: 0,
-        };
-
-        this.closeLightbox = this.closeLightbox.bind(this);
-        this.gotoNext = this.gotoNext.bind(this);
-        this.gotoPrevious = this.gotoPrevious.bind(this);
-        this.openLightbox = this.openLightbox.bind(this);
-        this.handleClickImage = this.handleClickImage.bind(this);
-
+    this.state = {
+      lightboxIsOpen: false,
+      currentImage: 0,
     }
 
-    openLightbox (index, event) {
-        event.preventDefault();
-        this.setState({
-            currentImage: index,
-            lightboxIsOpen: true,
-        });
-    }
-    closeLightbox () {
-        this.setState({
-            currentImage: 0,
-            lightboxIsOpen: false,
-        });
-    }
-    gotoPrevious () {
-        this.setState({
-            currentImage: this.state.currentImage - 1,
-        });
-    }
-    gotoNext () {
-        this.setState({
-            currentImage: this.state.currentImage + 1,
-        });
-    }
-    handleClickImage () {
-        if (this.state.currentImage === this.props.images.length - 1) return;
+    this.closeLightbox = this.closeLightbox.bind(this)
+    this.gotoNext = this.gotoNext.bind(this)
+    this.gotoPrevious = this.gotoPrevious.bind(this)
+    this.openLightbox = this.openLightbox.bind(this)
+    this.handleClickImage = this.handleClickImage.bind(this)
+  }
 
-        this.gotoNext();
-    }
+  openLightbox(index, event) {
+    event.preventDefault()
+    this.setState({
+      currentImage: index,
+      lightboxIsOpen: true,
+    })
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    })
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    })
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    })
+  }
+  handleClickImage() {
+    if (this.state.currentImage === this.props.images.length - 1) return
 
-    render() {
+    this.gotoNext()
+  }
 
-        const {site, galleryImages} = this.props.data;
+  render() {
+    const { site, galleryImages } = this.props.data
 
-        const siteTitle = site.siteMetadata.title;
-        const siteDescription = site.siteMetadata.description;
+    const siteTitle = site.siteMetadata.title
+    const siteDescription = site.siteMetadata.description
 
-        const images = galleryImages.edges;
-        images.map((img, index) => {
-            DEFAULT_IMAGES[index].src = img.node.childImageSharp.sizes.src;
-            DEFAULT_IMAGES[index].thumbnail = img.node.childImageSharp.resize.src;
-        });
+    const images = galleryImages.edges
+    images.map((img, index) => {
+      DEFAULT_IMAGES[index].src = img.node.childImageSharp.sizes.src
+      DEFAULT_IMAGES[index].thumbnail = img.node.childImageSharp.resize.src
+    })
 
+    return (
+      <div>
+        <Helmet>
+          <title>{siteTitle}</title>
+          <meta name="description" content={siteDescription} />
+        </Helmet>
 
-        
+        <div id="main">
+          <section id="one">
+            <header className="major">
+              <h2>Floripedia - Florile noastre, emotiile tale</h2>
+            </header>
+            <p>
+              Pentru că poveştile nu înseamnă doar cuvinte scrise pe pagini
+              albe. Poveştile noastre au culori, mirosuri şi sunt despre tine.
+              Buchetul ăla simplu ce mirosea a bujori şi a primăvară, pe care
+              l-ai ţinut în mână când ai intrat în biserică alături de el. Sau
+              explozia de flori colorate din ziua în care ai plâns de fericire.
+              Sau geanta plină de flori cu care ai atras toate privirile şi
+              toată admiraţia lumii.
+            </p>
+            <ul className="actions">
+              <li>
+                <a href="#three" className="button special big">
+                  Vreau un aranjament
+                </a>
+              </li>
+              `
+            </ul>
+          </section>
 
-        return (
-            <div>
-                <Helmet>
-                        <title>{siteTitle}</title>
-                        <meta name="description" content={siteDescription} />
-                </Helmet>
+          <section id="two">
+            <h2>Cele mai recente</h2>
 
-                <div id="main">
+            <Gallery
+              images={DEFAULT_IMAGES.map(
+                ({ id, src, thumbnail, caption, description }) => ({
+                  src,
+                  thumbnail,
+                  caption,
+                  description,
+                })
+              )}
+            />
 
-                    <section id="one">
-                        <header className="major">
-                            <h2>Ipsum lorem dolor aliquam ante commodo<br />
-                            magna sed accumsan arcu neque.</h2>
-                        </header>
-                        <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
-                        <ul className="actions">
-                            <li><a href="#" className="button">Learn More</a></li>
-                        </ul>
-                    </section>
+            <ul className="actions">
+              <li>
+                <a href="#" className="button">
+                  Full Portfolio
+                </a>
+              </li>
+            </ul>
+          </section>
 
-                    <section id="two">
-                        <h2>Recent Work</h2>
+          <section id="three">
+            <h2>Get In Touch</h2>
+            <p>
+              Poveştile noastre poartă culorile şi emoţiile tale. Florile
+              noastre nu durează o veşnice. Dar nu-i aşa ca de fiecare dată când
+              vei mirosi un bujor, îţi vei aminti de o emoţie a ta, prinsă de
+              noi într-un buchet creat special pentru tine?.
+            </p>
 
-                        <Gallery images={DEFAULT_IMAGES.map(({ id, src, thumbnail, caption, description }) => ({
-                            src,
-                            thumbnail,
-                            caption,
-                            description
-                        }))} />
+            <iframe
+              className="google-form"
+              src="https://docs.google.com/forms/d/e/1FAIpQLSeLPWexAmRphhVVpsfEhYwZE5a6td_RfjHvnf2i0zI4ZA_Mzw/viewform?embedded=true"
+              frameBorder="0"
+              marginHeight="0"
+              marginWidth="0"
+            >
+              Loading...
+            </iframe>
 
-                        <ul className="actions">
-                            <li><a href="#" className="button">Full Portfolio</a></li>
-                        </ul>
-                    </section>
-
-                    <section id="three">
-                        <h2>Get In Touch</h2>
-                        <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
-                        
-                        <iframe className="google-form" src="https://docs.google.com/forms/d/e/1FAIpQLSeLPWexAmRphhVVpsfEhYwZE5a6td_RfjHvnf2i0zI4ZA_Mzw/viewform?embedded=true" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
-                        
-                        {/* <div className="row">
+            {/* <div className="row">
                             <div className="8u 12u$(small)">
                                 <form method="post" action="#">
                                     <div className="row uniform 50%">
@@ -151,45 +217,37 @@ class HomeIndex extends React.Component {
                                 </ul>
                             </div>
                         </div> */}
-                    </section>
-
-                </div>
-
-            </div>
-        )
-    }
+          </section>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default HomeIndex
 
 export const pageQuery = graphql`
-    query PageQuery {
-        site {
-            siteMetadata {
-                title
-                description
-            }
-        }
-        galleryImages: allFile(
-            filter: {
-                relativePath:{
-                glob:"gallery/*.*"
-                }
-            }
-            ) {
-                edges{
-                node{
-                childImageSharp{
-                    resize(width: 350, height: 250 rotate: 180) {
-                        src
-                    },
-                    sizes(maxWidth: 800) {
-                        src
-                        }
-                    }
-                }
-            }
-            totalCount
-        }
+  query PageQuery {
+    site {
+      siteMetadata {
+        title
+        description
+      }
     }
+    galleryImages: allFile(filter: { relativePath: { glob: "gallery/*.*" } }) {
+      edges {
+        node {
+          childImageSharp {
+            resize(width: 350, height: 250, rotate: 180) {
+              src
+            }
+            sizes(maxWidth: 800) {
+              src
+            }
+          }
+        }
+      }
+      totalCount
+    }
+  }
 `
